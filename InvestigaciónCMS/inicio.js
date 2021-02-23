9ffd859315d8d742f1f6a30d49521f9a56ea8ab6
AndresC-O <62961669+AndresC-O@users.noreply.github.com>
2021-02-22 21:50:16 -0600

    Nueva capa de personalización

--- InvestigaciónCMS/inicio.js
+++ InvestigaciónCMS/inicio.js
@@ -1,17 +1,15 @@
-
 //Función que recibe un parámeto de píxeles.
 function irArriba(pxPantalla) {
-    window.addEventListener('scroll', () => {
-        var scroll = document.documentElement.scrollTop;
-        var botonArriba = document.getElementById('btnarriba');
+  window.addEventListener("scroll", () => {
+    var scroll = document.documentElement.scrollTop;
+    var botonArriba = document.getElementById("btnarriba");
 
-        if (scroll > pxPantalla) {
-            botonArriba.style.right = 20 + "px";
-        }
-        else {
-            botonArriba.style.right = -100 + "px";
-        }
-    })
+    if (scroll > pxPantalla) {
+      botonArriba.style.right = 20 + "px";
+    } else {
+      botonArriba.style.right = -100 + "px";
+    }
+  });
 }
 
 irArriba(150);
